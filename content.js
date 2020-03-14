810fb5123ae4e16a6816115f9938e1b131da1cdc
Libra694 <hashimoto.libra+development@gmail.com>
2020-03-15 02:06:14 +0900

    spaces to tabs

--- content.js
+++ content.js
@@ -1,73 +1,73 @@
 ﻿
 function zerofill(num) {
-    return ('0' + num).slice(-2);
+	return ('0' + num).slice(-2);
 }
 
 function date2str(date) {
 
-    var str = '';
-    str += date.getFullYear();
-    str += zerofill(date.getMonth() + 1);
-    str += zerofill(date.getDate());
-    str += 'T';
-    str += zerofill(date.getHours());
-    str += zerofill(date.getMinutes());
-    str += zerofill(date.getSeconds());
- 
-    return str;
+	var str = '';
+	str += date.getFullYear();
+	str += zerofill(date.getMonth() + 1);
+	str += zerofill(date.getDate());
+	str += 'T';
+	str += zerofill(date.getHours());
+	str += zerofill(date.getMinutes());
+	str += zerofill(date.getSeconds());
+
+	return str;
 }
 
-function calender(title, details, place, start_date, end_date, calender_id='') {
-    return 'http://www.google.com/calendar/event?' +
-        'action=' + 'TEMPLATE' +
-        '&text=' + encodeURIComponent(title) +
-        '&details=' + encodeURIComponent(details) +
-        '&location=' + encodeURIComponent(place) +
-        '&dates=' + date2str(start_date) + '/' + date2str(end_date) +
-        '&src=' + encodeURIComponent(calender_id) +
-        '&sprop=' + 'Libra694';
+function calender(title, details, place, start_date, end_date, calender_id = '') {
+	return 'http://www.google.com/calendar/event?' +
+		'action=' + 'TEMPLATE' +
+		'&text=' + encodeURIComponent(title) +
+		'&details=' + encodeURIComponent(details) +
+		'&location=' + encodeURIComponent(place) +
+		'&dates=' + date2str(start_date) + '/' + date2str(end_date) +
+		'&src=' + encodeURIComponent(calender_id) +
+		'&sprop=' + 'Libra694';
 }
 
 document.body.addEventListener('click', function (event) {
 
-    if (event.target.className == 'style-scope ytd-toggle-button-renderer style-compact-gray') {
+	if (event.target.className == 'style-scope ytd-toggle-button-renderer style-compact-gray') {
 
-        if ($(event.target.offsetParent).attr("aria-pressed") == "false") return;
+		if ($(event.target.offsetParent).attr("aria-pressed") == "false") return;
 
-        var $dismissable = $(event.target).closest('#dismissable');
-        var video_url = 'https://www.youtube.com' + $dismissable.find('#thumbnail').attr('href');
-        var title = $dismissable.find('#video-title').text().trim();
-        var channel = $dismissable.find('#channel-name #text').text().trim();
-        var date = $dismissable.find('#metadata-line').text().trim().split(/[ に]/).slice(0, 2).join(' ') + ':00';
-        
-        if (channel == '') channel = $('#channel-header #channel-name #text').text().trim();
-    }
-    else if (event.target.className == 'ytp-offline-slate-button-text' || event.target.className == 'ytp-offline-slate-button ytp-button') {
+		var $dismissable = $(event.target).closest('#dismissable');
+		var video_url = 'https://www.youtube.com' + $dismissable.find('#thumbnail').attr('href');
+		var title = $dismissable.find('#video-title').text().trim();
+		var channel = $dismissable.find('#channel-name #text').text().trim();
+		var date = $dismissable.find('#metadata-line').text().trim().split(/[ に]/).slice(0, 2).join(' ') + ':00';
 
-        if ($(event.target).text().indexOf('設定') >= 0) return;
+		if (channel == '') channel = $('#channel-header #channel-name #text').text().trim();
+	}
+	else if (event.target.className == 'ytp-offline-slate-button-text' || event.target.className == 'ytp-offline-slate-button ytp-button') {
 
-        var video_url = location.href;
-        var title = $('#info h1').text().trim();
-        var channel = $('#meta-contents #channel-name #text').text().trim();
-        var d = $('.ytp-offline-slate-subtitle-text').text().trim().split(/[年月日]/);
-        var date = (d.length == 3 ? new Date().getFullYear() : d[d.length - 4]) + '/' + d[d.length - 3] + '/' + d[d.length - 2] + d[d.length - 1] + ':00';
-    }
-    else return;
+		if ($(event.target).text().indexOf('設定') >= 0) return;
 
-    var start_date = new Date(date);
-    var end_date = new Date(date);
-    end_date.setHours(end_date.getHours() + 1);
+		var video_url = location.href;
+		var title = $('#info h1').text().trim();
+		var channel = $('#meta-contents #channel-name #text').text().trim();
+		var d = $('.ytp-offline-slate-subtitle-text').text().trim().split(/[年月日]/);
+		var date = (d.length == 3 ? new Date().getFullYear() : d[d.length - 4]) + '/' + d[d.length - 3] + '/' + d[d.length - 2] + d[d.length - 1] + ':00';
+	}
+	else return;
 
-    chrome.runtime.sendMessage(
-        "get_calendar_id",
-        function (response) {
-            if (response) {
-                var calender_url = calender(title, video_url, channel, start_date, end_date, calender_id = response);
-            } else {
-                var calender_url = calender(title, video_url, channel, start_date, end_date);
-            }
-            window.open(calender_url);
-        }
-    );
+	var start_date = new Date(date);
+	var end_date = new Date(date);
+	end_date.setHours(end_date.getHours() + 1);
+
+	chrome.runtime.sendMessage(
+		"get_calendar_id",
+		function (response) {
+			if (response) {
+				var calender_url = calender(title, video_url, channel, start_date, end_date, calender_id = response);
+			} else {
+				var calender_url = calender(title, video_url, channel, start_date, end_date);
+			}
+			window.open(calender_url);
+		}
+	);
 
 }, false)
--- options.js
+++ options.js
@@ -1,19 +1,19 @@
 
 function save_options() {
 
-    chrome.storage.sync.set({
-        calendar_id: document.getElementById('calendar_id').value
-    });
+	chrome.storage.sync.set({
+		calendar_id: document.getElementById('calendar_id').value
+	});
 }
 
 
 function restore_options() {
 
-    chrome.storage.sync.get({
-        calendar_id: null
-    }, function (items) {
-        document.getElementById('calendar_id').value = items.calendar_id;
-    });
+	chrome.storage.sync.get({
+		calendar_id: null
+	}, function (items) {
+		document.getElementById('calendar_id').value = items.calendar_id;
+	});
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
